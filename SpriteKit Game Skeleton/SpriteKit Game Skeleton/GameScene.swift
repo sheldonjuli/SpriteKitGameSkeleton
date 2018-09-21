5e3392beeafa7327ee1388e876088f961e8ee9aa
Li Ju <sheldon.juli@gmail.com>
2018-09-21 13:43:44 -0400

    Committed a empty game

---
+++ SpriteKit Game Skeleton/SpriteKit Game Skeleton/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  SpriteKit Game Skeleton
+//
+//  Created by Li Ju on 2018-09-21.
+//  Copyright © 2018 Li Ju. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+
+    override func didMove(to view: SKView) {
+
+    }
+}
